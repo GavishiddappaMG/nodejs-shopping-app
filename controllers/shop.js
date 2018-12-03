ba32cbd967e82e6083ebbc6b16158e9ca47f36db
gavishiddappa.gadogi <gavishiddappa.gadogi@impelsys.com>
2018-12-03 22:33:58 +0530

    initial commit

---
+++ controllers/shop.js
@@ -0,0 +1,87 @@
+
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/product-list',
+            {
+                prods: products,
+                pageTitle: 'All products',
+                path: '/products'
+            }
+        );
+    });
+};
+
+exports.getProduct = (req, res, next) => {
+    const productId = req.params.productId;
+    Product.findById(productId, product => {
+        res.render('shop/product-detail', {
+            product: product,
+            path: '/products',
+            pageTitle: product.title
+        })
+    });
+}
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/index',
+            {
+                prods: products,
+                pageTitle: 'Shop',
+                path: '/',
+            }
+        );
+    });
+};
+
+exports.getCart = (req, res, next) => {
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (let product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if (cartProductData) {
+                    cartProducts.push({ productData: product, qty: cartProductData.qty });
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        });
+    });
+};
+
+exports.postCart = (req, res, next) => {
+    const productId = req.body.productId;
+    Product.findById(productId, (product) => {
+        Cart.addProduct(productId, product.price);
+    });
+    res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    })
+};
+
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout'
+    });
+};
+
+exports.postCartDelete = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price);
+        res.redirect('/cart');
+    });
+};
---
+++ routes/shop.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const express = require('express');
+const router = express.Router();
+
+const shopController = require('../controllers/shop');
+
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/product/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDelete);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkouts', shopController.getCheckout);
+
+module.exports = router;
